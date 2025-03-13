5c363662b447bee77624e74eca378fd82cd19b1d
Rodney Kinney <rodney.m.kinney@gmail.com>
2025-03-13 06:13:43 -0700

    wip

--- src/cubelib/viz.py
+++ src/cubelib/viz.py
@@ -1,79 +1,9 @@
 import pygame
-import math
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-# Initialize pygame
-pygame.init()
-
-# Set up the display
-display_width = 800
-display_height = 600
-pygame.display.set_mode((display_width, display_height), DOUBLEBUF | OPENGL)
-pygame.display.set_caption("3D Grid of Colored Squares")
-
-# Set up the perspective
-glMatrixMode(GL_PROJECTION)
-gluPerspective(45, (display_width / display_height), 0.1, 50.0)
-
-glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-glEnable( GL_BLEND )
-
-# Initial camera position
-camera_x = 5.0
-camera_y = 5.0
-camera_z = 10.0
-
-# Define colors for the squares (RGB format)
-colors = [
-    (1.0, 0.0, 0.0, 0.3),  # Red
-    (0.0, 1.0, 0.0, 0.3),  # Green
-    (0.0, 0.0, 1.0, 0.3),  # Blue
-    (1.0, 1.0, 0.0, 0.3),  # Yellow
-    (1.0, 0.0, 1.0, 0.3),  # Magenta
-    (0.0, 1.0, 1.0, 0.3),  # Cyan
-    (0.7, 0.3, 0.5, 0.3),  # Purple
-    (1.0, 0.5, 0.0, 0.3),  # Orange
-    (0.5, 0.5, 0.5, 0.3)  # Gray
-]
-
-
-def draw_square(x, y, z, color_idx, axis='xy'):
-    """Draw a square at given position with specified color and axis"""
-    # if color_idx not in {2, 7}:
-    #     return
-
-    color = colors[color_idx]
-
-    glPushMatrix()
-    glTranslatef(x, y, z)
-    glColor4fv(color)
-
-    # Draw a square face
-    glBegin(GL_QUADS)
-    if axis == 'xy':
-        glVertex3f(-0.5, -0.5, 0.0)
-        glVertex3f(0.5, -0.5, 0.0)
-        glVertex3f(0.5, 0.5, 0.0)
-        glVertex3f(-0.5, 0.5, 0.0)
-    elif axis == 'xz':
-        glVertex3f(-0.5, 0.0, -0.5)
-        glVertex3f(0.5, 0.0, -0.5)
-        glVertex3f(0.5, 0.0, 0.5)
-        glVertex3f(-0.5, 0.0, 0.5)
-    elif axis == 'yz':
-        glVertex3f(0.0, -0.5, -0.5)
-        glVertex3f(0.0, 0.5, -0.5)
-        glVertex3f(0.0, 0.5, 0.5)
-        glVertex3f(0.0, -0.5, 0.5)
-    glEnd()
-
-    glPopMatrix()
-
-    # U + L + F + R + B + D
-
-
+# U + L + F + R + B + D
 facelet_x = \
     [-1, 0, 1, -1, 0, 1, -1, 0, 1] \
     + [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5] \
@@ -84,9 +14,9 @@ facelet_x = \
 facelet_y = \
     [-1, -1, -1, 0, 0, 0, 1, 1, 1] \
     + [-1, 0, 1, -1, 0, 1, -1, 0, 1] \
-    + [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5] \
-    + [1, 0, -1, 1, 0, -1, 1, 0, -1] \
     + [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5] \
+    + [1, 0, -1, 1, 0, -1, 1, 0, -1] \
+    + [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5] \
     + [-1, -1, -1, 0, 0, 0, 1, 1, 1]
 facelet_z = \
     [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5] \
@@ -103,29 +33,137 @@ axis = ["xy"] * 9 \
        + ["xz"] * 9 \
        + ["xy"] * 9
 
+WHITE = (1, 1, 1)
+YELLOW = (1, 1, 0)
+GREEN = (0, 1, 0)
+BLUE = (0, 0, 1)
+RED = (1, 0, 0)
+ORANGE = (1, .5, 0)
 
-def draw_grid():
-    """Draw a 3x3 grid of squares"""
-    color_idx = 0
-    for i in range(0,54):
-        draw_square(facelet_x[i], facelet_y[i], facelet_z[i], color_idx, axis[i])
-        # for a in range(-1, 2):
-        #     for b in range(-1, 2):
-        #         draw_square(b, -1.5, a, color_idx, "xz")
-        #         draw_square(b, 1.5, a, color_idx, "xz")
-        #         draw_square(-1.5, a, b, color_idx, "yz")
-        #         draw_square(1.5, a, b, color_idx, "yz")
-        #         draw_square(a, b, -1.5, color_idx, "xy")
-        #         draw_square(a, b, 1.5, color_idx, "xy")
-        color_idx = (color_idx + 1) % len(colors)  # Cycle through colors
+
+class CubeViz():
+    """Visualize a cube in 3D space using pygame and OpenGL"""
+
+    def __init__(
+            self,
+            width=800,
+            height=600,
+            opacity=.6,  # Set the opacity for the colors
+    ):
+        # Set up the display
+        self.display_width = width
+        self.display_height = height
+        self.opacity = opacity
+        pygame.display.set_mode((self.display_width, self.display_height), DOUBLEBUF | OPENGL)
+        pygame.display.set_caption("3D Grid of Colored Squares")
+
+        # Set up the perspective
+        glMatrixMode(GL_PROJECTION)
+        gluPerspective(45, (self.display_width / self.display_height), 0.1, 50.0)
+
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_BLEND)
+
+        # Initial camera position
+        self.camera_x = 0.0
+        self.camera_y = -10.0
+        self.camera_z = 6.0
+        self.rotation_angle = -30
+
+        self.colors = [WHITE] * 9 \
+                      + [ORANGE] * 9 \
+                      + [GREEN] * 9 \
+                      + [RED] * 9 \
+                      + [BLUE] * 9 \
+                      + [YELLOW] * 9
+
+    def draw_facelet(self, x, y, z, color, axis='xy'):
+        """Draw a single face of the cube"""
+
+        glPushMatrix()
+        glTranslatef(x, y, z)
+        glColor4fv(color)
+
+        # Draw a square face
+        glBegin(GL_QUADS)
+        if axis == 'xy':
+            glVertex3f(-0.48, -0.48, 0.0)
+            glVertex3f(0.48, -0.48, 0.0)
+            glVertex3f(0.48, 0.48, 0.0)
+            glVertex3f(-0.48, 0.48, 0.0)
+        elif axis == 'xz':
+            glVertex3f(-0.48, 0.0, -0.48)
+            glVertex3f(0.48, 0.0, -0.48)
+            glVertex3f(0.48, 0.0, 0.48)
+            glVertex3f(-0.48, 0.0, 0.48)
+        elif axis == 'yz':
+            glVertex3f(0.0, -0.48, -0.48)
+            glVertex3f(0.0, 0.48, -0.48)
+            glVertex3f(0.0, 0.48, 0.48)
+            glVertex3f(0.0, -0.48, 0.48)
+        glEnd()
+
+        glPopMatrix()
+
+    def draw(self):
+        # Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Set camera position
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(self.camera_x, self.camera_y, self.camera_z,  # Camera position
+                  0, 0, 0,  # Look at point
+                  0, 1, 0)  # Up vector
+
+        glRotatef(self.rotation_angle, 0, 0, 1)
+
+        """Draw facelets from back to front"""
+        # D
+        for i in range(45, 54):
+            self.draw_facelet(facelet_x[i], facelet_y[i], facelet_z[i],
+                              self.colors[i] + (self.opacity,), axis[i])
+        # B
+        for i in range(36,45):
+            self.draw_facelet(facelet_x[i], facelet_y[i], facelet_z[i],
+                              self.colors[i] + (self.opacity,), axis[i])
+        # L
+        for i in range(9, 18):
+            self.draw_facelet(facelet_x[i], facelet_y[i], facelet_z[i],
+                              self.colors[i] + (self.opacity,), axis[i])
+        # R
+        for i in range(27,36):
+            self.draw_facelet(facelet_x[i], facelet_y[i], facelet_z[i],
+                              self.colors[i] + (self.opacity,), axis[i])
+
+        # F
+        for i in range(18,27):
+            self.draw_facelet(facelet_x[i], facelet_y[i], facelet_z[i],
+                              self.colors[i] + (self.opacity,), axis[i])
+        # U
+        for i in range(0, 9):
+            self.draw_facelet(facelet_x[i], facelet_y[i], facelet_z[i],
+                              self.colors[i] + (self.opacity,), axis[i])
+
+    def rotate(self, angle):
+        self.rotation_angle += angle
+        print(f"Rotation angle: {self.rotation_angle}")
+
+    def move_camera(self, dx, dy, dz):
+        """Move the camera position"""
+        self.camera_x += dx
+        self.camera_y += dy
+        self.camera_z += dz
+        print(f"Camera position: ({self.camera_x}, {self.camera_y}, {self.camera_z})")
 
 
 def main():
-    global camera_x, camera_y, camera_z
+    # Initialize pygame
+    pygame.init()
 
+    viz = CubeViz()
     clock = pygame.time.Clock()
     running = True
-    rotation_angle = 0
 
     while running:
         for event in pygame.event.get():
@@ -134,35 +172,18 @@ def main():
 
             # Camera position controls
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    camera_x -= 0.5
-                if event.key == pygame.K_RIGHT:
-                    camera_x += 0.5
                 if event.key == pygame.K_UP:
-                    camera_y += 0.5
+                    #viz.move_camera(0, 0, 2)
+                    viz.rotate(-4)
                 if event.key == pygame.K_DOWN:
-                    camera_y -= 0.5
-                if event.key == pygame.K_w:
-                    camera_z -= 0.5
-                if event.key == pygame.K_s:
-                    camera_z += 0.5
+                    #viz.move_camera(0, 0, -2)
+                    viz.rotate(4)
+                if event.key == pygame.K_LEFT:
+                    viz.move_camera(-2, 0, 0)
+                if event.key == pygame.K_RIGHT:
+                    viz.move_camera(2, 0, 0)
 
-        # Clear the screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        # Set camera position
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        gluLookAt(camera_x, camera_y, camera_z,  # Camera position
-                  0, 0, 0,  # Look at point
-                  0, 1, 0)  # Up vector
-
-        # Slowly rotate the view
-        rotation_angle += 0.5
-        glRotatef(rotation_angle, 0, 1, 0)
-
-        # Draw the grid
-        draw_grid()
+        viz.draw()
 
         # Update the display
         pygame.display.flip()
